7ec32a46d8f35985f6de8dde3e0ca9023f32a67f
Josep M. Pujol <josep@3scale.net>
2011-03-28 13:18:38 +0200

    change user methods, for the version

--- lib/3scale/core/user.rb
+++ lib/3scale/core/user.rb
@@ -10,9 +10,9 @@ module ThreeScale
       attr_accessor :plan_name
       attr_accessor :version
       
-      def self.load!(service, username)
+      def self.load(service, username)
         
-        key = self.storage_key(service.id, username)
+        key = self.key(service.id, username)
 
         values = storage.hmget(key,"state","plan_id","plan_name")
         state, plan_id, plan_name = values
@@ -23,14 +23,14 @@ module ThreeScale
           return nil if service.user_registration_required?
           ## the user did not exist and we need to create it
 
-          user = User.new(:service_id => service.id,
+          user = new(:service_id => service.id,
                      :username   => username,
                      :state      => :active.to_sym,
                      :plan_id    => service.default_user_plan_id,
                      :plan_name  => service.default_user_plan_name) 
           user.save
         else 
-          user = User.new(:service_id => service.id,
+          user = new(:service_id => service.id,
                           :username   => username,
                           :state      => state.to_sym,
                           :plan_id    => plan_id,
@@ -47,63 +47,58 @@ module ThreeScale
       end
 
       def save  
-        key = storage_key
+
         storage.hset(key,"state", state.to_s) if state
         storage.hset(key,"plan_id", plan_id)     if plan_id
         storage.hset(key,"plan_name", plan_name) if plan_name
         storage.hset(key,"username", username) if username
         storage.hset(key,"service_id", service_id) if service_id
-        storage.incrby("#{key}/version",1)
-        storage_key(service_id, username)
+        storage.incrby(storage_key(service_id, username, :version),1)
+        
         Service.incr_version(service_id)
       end
 
       def self.get_version(service_id, username)
-        storage.get(storage_key(service_id, username),"/version")
+        storage.get(storage_key(service_id, username,:version))
       end
 
       def self.incr_version(service_id, username)
-        storage.incrby(storage_key(service_id, username),"/version",1)
+        storage.incrby(storage_key(service_id, username,:version),1)
       end
 
       def self.delete(service_id, username)
-        key = storage_key(service_id, username)
+        key = self.key(service_id, username)
         storage.hdel(key,"state")
         storage.hdel(key,"plan_id")
         storage.hdel(key,"plan_name")
         storage.hdel(key,"username")
         storage.hdel(key,"service_id")
-        storage.del("#{key}/version",1)
+        storage.del(storage_key(service_id, username,:version))
+
         Service.incr_version(service_id)
       end
       
-      def metric_names
-        @metric_names ||= {}
-      end
-
-      def metric_names=(hash)
-        @metric_names = hash
-      end
-
-      def metric_name(metric_id)
-        metric_names[metric_id] ||= Metric.load_name(service_id, metric_id)
-      end
-
-      def usage_limits
-        @usage_limits ||= UsageLimit.load_all(service_id, plan_id)
-      end
-
       def active?
         state == :active
       end
 
-      def storage_key
-          self.class.storage_key(service_id,username)
+      def key
+          self.class.key(service_id,username)
       end
 
-      def self.storage_key(service_id, username)
+      def self.key(service_id, username)
          "service:#{service_id}/user:#{username}"
       end
+
+      def storage_key(service_id, username, attribute)
+         self.class.storage_key(service_id, username, attribute)
+      end
+
+      def self.storage_key(service_id, username, attribute)
+         "service:#{service_id}/user:#{username}/#{attribute}"
+      end
+
+
     end
   end
 end
