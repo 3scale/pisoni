d093d7d419f4d91cc74a859855f8e9f21faa2a96
Alejandro Martinez Ruiz <alex@flawedcode.org>
2014-10-06 16:16:58 +0200

    metric: convert to use the internal API
    This also drops methods not used in Core nor in System.

--- lib/3scale/core/metric.rb
+++ lib/3scale/core/metric.rb
@@ -1,105 +1,45 @@
 module ThreeScale
   module Core
-    class Metric
-      include Storable
+    class Metric < APIClient::Resource
+      attributes :service_id, :id, :parent_id, :name
 
-      attr_accessor :service_id
-      attr_accessor :id
-      attr_accessor :parent_id
-      attr_accessor :name
+      def self.base_uri
+        '/internal/services'
+      end
 
-      def self.load_all_ids(service_id)
-        storage.smembers(id_set_key(service_id)) || []
+      def base_uri
+        self.class.base_uri
       end
 
       def self.load(service_id, id)
-        name, parent_id = storage.mget(key(service_id, id, :name),
-                                       key(service_id, id, :parent_id))
-
-        name && new(:id         => id.to_s,
-                    :service_id => service_id.to_s,
-                    :name       => name,
-                    :parent_id  => parent_id)
-      end
-
-      def self.load_all_names(service_id, ids)
-        Hash[ids.zip(storage.mget(*ids.map{|id| key(service_id, id, :name)}))]
-      end
-
-      def self.load_name(service_id, id)
-        storage.get(key(service_id, id, :name))
-      end
-
-      def self.load_id(service_id, name)
-        storage.get(id_key(service_id, name))
+        api_read({}, uri: "#{base_uri}/#{service_id}/metrics/#{id}")
       end
 
       def self.save(attributes)
-        metrics = new(attributes)
-        metrics.save
-        metrics
+        service_id, id = attributes[:service_id], attributes[:id]
+        api_create attributes, uri: "#{base_uri}/#{service_id}/metrics/#{id}"
       end
 
       def self.delete(service_id, id)
-        name = load_name(service_id, id)
-
-        storage.srem(id_set_key(service_id), id)
-
-        storage.del(key(service_id, id, :name))
-        storage.del(key(service_id, id, :parent_id))
-        storage.del(id_key(service_id, name))
-
-        Service.incr_version(service_id)
+        api_delete({}, uri: "#{base_uri}/#{service_id}/metrics/#{id}")
       end
 
-      def save
-        storage.set(id_key(service_id, name), id)
-        storage.set(key(service_id, id, :name), name)
-        storage.set(key(service_id, id, :parent_id), parent_id) if parent_id
-
-        storage.sadd(id_set_key(service_id), id)
-
-        save_children
-        Service.incr_version(service_id)
+      def self.load_name(service_id, id)
+        ret = api_do_get({}, uri: "#{base_uri}/#{service_id}/metrics/#{id}")
+        ret[:attributes][:name] if ret[:ok]
       end
 
-      def children
-        @children ||= []
+      def self.load_id(service_id, name)
+        ret = api_do_get({}, uri: "#{base_uri}/#{service_id}/metrics/name/#{name}")
+        ret[:attributes][:id] if ret[:ok]
       end
 
-      attr_writer :children
-
-      module KeyHelpers
-        def key(service_id, id, attribute)
-          encode_key("metric/service_id:#{service_id}/id:#{id}/#{attribute}")
-        end
-
-        def id_key(service_id, name)
-          encode_key("metric/service_id:#{service_id}/name:#{name}/id")
-        end
-
-        def id_set_key(service_id)
-          encode_key("metrics/service_id:#{service_id}/ids")
-        end
-
-        def metric_names_key(service_id)
-          encode_key("metrics/service_id:#{service_id}/metric_names")
-        end
-
+      # XXX depends on UsageLimit
+      def self.load_all_ids(service_id)
+        ret = api_do_get({}, uri: "#{base_uri}/#{service_id}/metrics/all")
+        ret[:attributes][:ids] if ret[:ok]
       end
 
-      include KeyHelpers
-      extend  KeyHelpers
-
-      private
-
-      def save_children
-        children.each do |child|
-          child.service_id = service_id
-          child.parent_id  = id
-          child.save
-        end
-      end
     end
   end
 end
