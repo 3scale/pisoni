d5d89787a3d318e3f97989dde8cb650cc89a3b1e
Alejandro Martinez Ruiz <alex@flawedcode.org>
2014-10-06 16:45:40 +0200

    test/usagelimits: fix tests due to dependency on Metric
    This adds VCR HTTP transactions to UsageLimits because of the
    dependency on Metric, so that they pass. This is temporary until
    UsageLimits is ported to the internal API.

--- test/usage_limit_test.rb
+++ test/usage_limit_test.rb
@@ -14,7 +14,7 @@ class UsageLimitTest < Test::Unit::TestCase
                     :day        => 45000,
                     :hour       => 2000,
                     :minute     => 10)
-    
+
     assert_equal '1000000',
                  storage.get('usage_limit/service_id:2001/plan_id:3001/metric_id:4001/month')
 
@@ -32,19 +32,27 @@ class UsageLimitTest < Test::Unit::TestCase
   end
 
   def test_load_all
-    Metric.save(:service_id => 2001, :id => 4001, :name => 'hits')
-    Metric.save(:service_id => 2001, :id => 4002, :name => 'transfer')
+    VCR.use_cassette 'create hits metric' do
+      Metric.save(:service_id => 2001, :id => 4001, :name => 'hits')
+    end
+    VCR.use_cassette 'create transfer metric' do
+      Metric.save(:service_id => 2001, :id => 4002, :name => 'transfer')
+    end
 
     storage.set('usage_limit/service_id:2001/plan_id:3001/metric_id:4001/month', 1000)
     storage.set('usage_limit/service_id:2001/plan_id:3001/metric_id:4001/week',  500)
     storage.set('usage_limit/service_id:2001/plan_id:3001/metric_id:4002/month', 2100)
 
-    usage_limits = UsageLimit.load_all(2001, 3001)
+    usage_limits = VCR.use_cassette 'UsageLimit.load_all' do
+      UsageLimit.load_all(2001, 3001)
+    end
     assert_equal 3, usage_limits.count
   end
 
   def test_load_all_returns_empty_array_if_there_are_no_metrics
-    usage_limits = UsageLimit.load_all(2001, 3001)
+    usage_limits = VCR.use_cassette 'UsageLimit.load_all w/o metrics' do
+      UsageLimit.load_all(2001, 3001)
+    end
     assert usage_limits.empty?, 'Expected usage_limits to be empty'
   end
 
@@ -62,7 +70,9 @@ class UsageLimitTest < Test::Unit::TestCase
   end
 
   def test_delete
-    Metric.save(:service_id => 2001, :id => 4001, :name => 'hits')
+    VCR.use_cassette 'create hits metric' do
+      Metric.save(:service_id => 2001, :id => 4001, :name => 'hits')
+    end
     UsageLimit.save(:service_id => 2001,
                     :plan_id    => 3001,
                     :metric_id  => 4001,
@@ -72,26 +82,37 @@ class UsageLimitTest < Test::Unit::TestCase
 
     assert_nil UsageLimit.load_value(2001, 3001, 4001, :minute)
 
-    usage_limits = UsageLimit.load_all(2001, 3001)
+    usage_limits = VCR.use_cassette 'UsageLimit.load_all in test_delete' do
+      UsageLimit.load_all(2001, 3001)
+    end
     assert usage_limits.none? { |limit| limit.metric_id == '4001' && limit.period == :minute }
   end
 
   def test_metric_name
-    Metric.save(:service_id => 2001, :id => 4001, :name => 'hits')
+    VCR.use_cassette 'create hits metric' do
+      Metric.save(:service_id => 2001, :id => 4001, :name => 'hits')
+    end
     usage_limit = UsageLimit.new(:service_id => '2001',
                                  :plan_id    => '3001',
                                  :metric_id  => '4001')
 
-    assert_equal 'hits', usage_limit.metric_name
+    metric_name = VCR.use_cassette 'get metric name' do
+      usage_limit.metric_name
+    end
+    assert_equal 'hits', metric_name
   end
 
   def test_value_is_numeric
-    Metric.save(:service_id => 2001, :id => 4001, :name => 'hits')
+    VCR.use_cassette 'create hits metric' do
+      Metric.save(:service_id => 2001, :id => 4001, :name => 'hits')
+    end
     UsageLimit.save(:service_id => '2001',
                     :plan_id    => '3001',
                     :metric_id  => '4001',
                     :month      => 1000000)
-    usage_limit = UsageLimit.load_all(2001, 3001).first
+    usage_limit = VCR.use_cassette 'UsageLimit.load_all in test_value_is_numeric' do
+      UsageLimit.load_all(2001, 3001).first
+    end
 
     assert_equal 1000000, usage_limit.value
   end
@@ -102,7 +123,7 @@ class UsageLimitTest < Test::Unit::TestCase
                     :metric_id  => '4001',
                     :month      => 1000000,
                     :eternity   => 300000)
-    
+
     assert_equal '1000000',
                  storage.get('usage_limit/service_id:2001/plan_id:3001/metric_id:4001/month')
 
@@ -112,7 +133,9 @@ class UsageLimitTest < Test::Unit::TestCase
   end
 
   def test_delete_with_eternity
-    Metric.save(:service_id => 2001, :id => 4001, :name => 'hits')
+    VCR.use_cassette 'create hits metric' do
+      Metric.save(:service_id => 2001, :id => 4001, :name => 'hits')
+    end
     UsageLimit.save(:service_id => 2001,
                     :plan_id    => 3001,
                     :metric_id  => 4001,
@@ -123,7 +146,9 @@ class UsageLimitTest < Test::Unit::TestCase
 
     assert_nil UsageLimit.load_value(2001, 3001, 4001, :eternity)
 
-    usage_limits = UsageLimit.load_all(2001, 3001)
+    usage_limits = VCR.use_cassette 'UsageLimit.load_all in test_delete_with_eternity' do
+      UsageLimit.load_all(2001, 3001)
+    end
     assert usage_limits.none? { |limit| limit.metric_id == '4001' && limit.period == :eternity }
 
     assert_equal usage_limits.first.period, :minute
