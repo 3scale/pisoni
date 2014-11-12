15bb52c723990220bc0ab908a1d804880cf3a403
Alejandro Martinez Ruiz <alex@flawedcode.org>
2014-11-12 16:05:59 +0100

    spec/usagelimits: add raise expectations for save

--- spec/usagelimit_spec.rb
+++ spec/usagelimit_spec.rb
@@ -7,8 +7,10 @@ module ThreeScale
         @sample_period_limits = { eternity: 10_000_000, year: 2_000_000, month: 700_000,
                                   week: 300_000, hour: 5_000, minute: 200 }
         VCR.use_cassette 'save a sample usage limit' do
-          UsageLimit.save({ service_id: '2001', plan_id: '3001', metric_id: '4001' }.
-                          merge(@sample_period_limits))
+          @sample_period_limits.each do |p, l|
+            UsageLimit.save(service_id: '2001', plan_id: '3001',
+                            metric_id: '4001', p => l)
+          end
         end
       end
 
@@ -47,6 +49,18 @@ module ThreeScale
         VCR.use_cassette 'load deleted period from sample usage limit' do
           UsageLimit.load_value('2001', '3001', '4001', :week)
         end.must_be_nil
+      end
+
+      it 'raises if multiple periods are specified on .save' do
+        proc do
+          UsageLimit.save(service_id: '2001', plan_id: '3001', metric_id: '4002', day: 5, week: 10)
+        end.must_raise UsageLimitInvalidPeriods
+      end
+
+      it 'raises if no periods are specified on .save' do
+        proc do
+          UsageLimit.save(service_id: '2001', plan_id: '3001', metric_id: '4002')
+        end.must_raise UsageLimitInvalidPeriods
       end
     end
   end
