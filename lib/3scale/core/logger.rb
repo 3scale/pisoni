690e3de1bfc4ecd77dbbbfbc25ae5823225d50db
Alejandro Martinez Ruiz <alex@flawedcode.org>
2015-01-20 16:30:45 +0100

    logger: declare usage of error loglevel
    This is in preparation of changes in api_client that will log
    errors.

--- lib/3scale/core/logger.rb
+++ lib/3scale/core/logger.rb
@@ -3,7 +3,7 @@ require 'injectedlogger'
 module ThreeScale
   module Core
     module Logger
-      InjectedLogger.use(:info, :debug) {}
+      InjectedLogger.use(:error, :info, :debug) {}
       InjectedLogger.after_injection do |logger|
         logger.prefix = '[core]'
       end
