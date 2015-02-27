7d7210f2ca2621703974fce55beaf8d0d07b2254
Alejandro Martinez Ruiz <alex@flawedcode.org>
2015-02-27 14:26:02 +0100

    storable: drop storage methods

--- lib/3scale/core/storable.rb
+++ lib/3scale/core/storable.rb
@@ -2,23 +2,9 @@ module ThreeScale
   module Core
     # Mix this into objects that should be storable in the storage.
     module Storable
-      def self.included(base)
-        base.extend(ClassMethods)
-      end
-
       def initialize(attributes = {})
         attributes.each do |key, value|
           send("#{key}=", value)
-        end
-      end
-
-      def storage
-        self.class.storage
-      end
-
-      module ClassMethods
-        def storage
-          Core.storage
         end
       end
     end
