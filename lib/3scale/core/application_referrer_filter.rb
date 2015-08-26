0d8dbc4b66cda2830342502172d4acf441c05505
Wojciech Ogrodowczyk <wojciech@haikuco.de>
2015-08-26 15:46:49 +0200

    Add support for application referrer filters API

---
+++ lib/3scale/core/application_referrer_filter.rb
@@ -0,0 +1,41 @@
+module ThreeScale
+  module Core
+    class ApplicationReferrerFilter < APIClient::Resource
+      attributes :service_id, :application_id, :value
+
+      default_uri '/internal/services/'
+
+      def self.load_all service_id, application_id
+        results = api_do_get({},
+          rprefix: :referrer_filters,
+          uri: base_uri(service_id, application_id)
+        )
+        results[:attributes]
+      end
+
+      def self.create service_id, application_id, value
+        api_create(
+          {referrer_filter: value},
+          uri: base_uri(service_id, application_id),
+          prefix: '',
+        )
+      end
+
+      def self.remove service_id, application_id, value
+        encoded_value = Base64.urlsafe_encode64(value)
+        api_do_delete(
+          {},
+          uri: base_uri(service_id, application_id) + "/#{encoded_value}",
+          prefix: ''
+        )
+      end
+
+      def self.base_uri service_id, application_id
+        "#{default_uri}#{service_id}/applications/#{application_id}/referrer_filters"
+      end
+      private_class_method :base_uri
+
+    end
+  end
+end
+
