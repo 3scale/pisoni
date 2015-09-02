c3749b4d23bfc25832bcaacbb3d00060af2946d5
Toni Reina <areina0@gmail.com>
2015-09-02 21:21:12 +0100

    Don't set default record setting for vcr

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,9 +22,7 @@ VCR.configure do |c|
   #c.debug_logger = File.open('vcr_debug.log', 'w')
   VCRFilteredSerializer.serializer = c.cassette_serializers[:yaml]
   c.cassette_serializers[:filtered] = VCRFilteredSerializer
-  full_build = ENV['FULL_BUILD'] == '1'
   c.default_cassette_options = { allow_playback_repeats: true,
-                                 record: full_build ? :all : :new_episodes,
                                  serialize_with: :filtered,
                                  match_requests_on: [:method, :path, :query, :body]
                                }
