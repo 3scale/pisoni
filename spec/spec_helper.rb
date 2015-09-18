3bb3eb93c22008bd5bfac773a20b4e8f38a66361
David Ortiz <z.david.ortiz@gmail.com>
2015-09-18 12:12:17 +0200

    Delete everything VCR-related from specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,24 +12,4 @@ $:.unshift(File.dirname(__FILE__) + '/../lib')
 
 require 'minitest/autorun'
 require 'bundler/setup'
-require_relative 'vcr_filtered_serializer'
-
 Bundler.require(:default, :development, :test)
-
-VCR.configure do |c|
-  c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
-  c.hook_into :faraday
-  #c.debug_logger = File.open('vcr_debug.log', 'w')
-  VCRFilteredSerializer.serializer = c.cassette_serializers[:yaml]
-  c.cassette_serializers[:filtered] = VCRFilteredSerializer
-  c.default_cassette_options = { allow_playback_repeats: true,
-                                 serialize_with: :filtered,
-                                 match_requests_on: [:method, :path, :query, :body]
-                               }
-  c.ignore_request do |request|
-    (URI(request.uri).path =~ /.*/)
-  end
-
-  # ignore requests to CodeClimate
-  c.ignore_hosts 'codeclimate.com'
-end
