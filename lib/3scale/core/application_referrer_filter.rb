e9e1cb4d2ab1c851cc6818cb8380d702c8ad8686
David Ortiz <z.david.ortiz@gmail.com>
2015-10-08 11:08:49 +0200

    ApplicationReferrerFilter: minor style changes

--- lib/3scale/core/application_referrer_filter.rb
+++ lib/3scale/core/application_referrer_filter.rb
@@ -7,28 +7,25 @@ module ThreeScale
 
       default_uri '/internal/services/'
 
-      def self.load_all service_id, application_id
+      def self.load_all(service_id, application_id)
         results = api_do_get({},
-          rprefix: :referrer_filters,
-          uri: base_uri(service_id, application_id)
-        )
+                             rprefix: :referrer_filters,
+                             uri: base_uri(service_id, application_id))
         results[:attributes]
       end
 
-      def self.save service_id, application_id, value
-        api_save(
-          {referrer_filter: value},
-          uri: base_uri(service_id, application_id),
-          prefix: '',
-        )
+      def self.save(service_id, application_id, value)
+        api_save({ referrer_filter: value },
+                 uri: base_uri(service_id, application_id),
+                 prefix: '',)
       end
 
-      def self.delete service_id, application_id, value
+      def self.delete(service_id, application_id, value)
         encoded_value = Base64.urlsafe_encode64(value)
         api_delete({}, uri: base_uri(service_id, application_id) + "/#{encoded_value}")
       end
 
-      def self.base_uri service_id, application_id
+      def self.base_uri(service_id, application_id)
         "#{default_uri}#{service_id}/applications/#{application_id}/referrer_filters"
       end
       private_class_method :base_uri
