465cf896aa0cc18aa4b9797c60bbc9a5aa07f6d6
David Ortiz <z.david.ortiz@gmail.com>
2015-10-08 11:27:03 +0200

    ApplicationReferrerFilter: delete unnecessary attributes
    There are three attributes declared in ApplicationReferrerFilter.
    However, none of those are returned in any call to backend. When
    calling save or load operations, backend only returns an unamed
    String for each filter, therefore, we cannot declare any attrs.
    ApplicationReferrerFilter.save was returning an object with
    the 3 attrs declared but all of them set to nil because, as
    mentioned above, backend does not return those attrs.

--- lib/3scale/core/application_referrer_filter.rb
+++ lib/3scale/core/application_referrer_filter.rb
@@ -3,8 +3,6 @@ require 'base64'
 module ThreeScale
   module Core
     class ApplicationReferrerFilter < APIClient::Resource
-      attributes :service_id, :application_id, :value
-
       default_uri '/internal/services/'
 
       def self.load_all(service_id, application_id)
@@ -15,9 +13,10 @@ module ThreeScale
       end
 
       def self.save(service_id, application_id, value)
-        api_save({ referrer_filter: value },
-                 uri: base_uri(service_id, application_id),
-                 prefix: '',)
+        result = api_do_post({ referrer_filter: value },
+                             uri: base_uri(service_id, application_id),
+                             prefix: '')
+        result[:response_json][:referrer_filter]
       end
 
       def self.delete(service_id, application_id, value)
@@ -29,7 +28,6 @@ module ThreeScale
         "#{default_uri}#{service_id}/applications/#{application_id}/referrer_filters"
       end
       private_class_method :base_uri
-
     end
   end
 end
