eb285c638f8aee7c769c98b3764a292dfc76b175
David Ortiz <z.david.ortiz@gmail.com>
2019-06-28 15:48:35 +0200

    spec/application_key: avoid duplication in #delete tests
    Also fix a typo in one of the tests that said that it should return true
    when the test should return false.

--- spec/application_key_spec.rb
+++ spec/application_key_spec.rb
@@ -75,47 +75,37 @@ module ThreeScale
       end
 
       describe '.delete' do
+        let(:service_id) { 300 }
+        let(:app_id) { 200 }
+        let(:key) { 'foo' }
+
+        before do
+          Application.save service_id: service_id, id: app_id, state: 'suspended',
+                           plan_id: '3066', plan_name: 'crappy', redirect_url: 'blah'
+        end
+
         describe 'with an existing application key' do
-          let(:service_id) { 300 }
-          let(:app_id)     { 200 }
-          let(:value)      { "foo" }
-
           before do
-            Application.save service_id: service_id, id: app_id, state: 'suspended',
-                             plan_id: '3066', plan_name: 'crappy', redirect_url: 'blah'
-
-            ApplicationKey.save(service_id, app_id, value)
+            ApplicationKey.save(service_id, app_id, key)
           end
 
           it 'returns true' do
-            ApplicationKey.delete(service_id, app_id, value).must_equal true
+            ApplicationKey.delete(service_id, app_id, key).must_equal true
           end
         end
 
         describe 'with a non-existing application key' do
-          let(:service_id) { 300 }
-          let(:app_id)     { 500 }
-          let(:value)      { "nonexistingkey" }
+          let(:key) { 'non_existing_key' }
 
-          before do
-            Application.save service_id: service_id, id: app_id, state: 'suspended',
-                             plan_id: '3066', plan_name: 'crappy', redirect_url: 'blah'
-          end
-
-          it 'returns true' do
-            ApplicationKey.delete(service_id, app_id, value).must_equal false
+          it 'returns false' do
+            ApplicationKey.delete(service_id, app_id, key).must_equal false
           end
         end
 
         describe 'with a key that contains special chars (*, _, etc.)' do
-          let(:service_id) { 300 }
-          let(:app_id)     { 500 }
-          let(:key)        { '#$*' }
+          let(:key_with_special_chars) { '#$*' }
 
           before do
-            Application.save service_id: service_id, id: app_id, state: 'suspended',
-                             plan_id: '3066', plan_name: 'crappy', redirect_url: 'blah'
-
             ApplicationKey.save(service_id, app_id, key)
           end
 
